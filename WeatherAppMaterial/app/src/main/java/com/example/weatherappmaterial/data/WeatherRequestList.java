67bd7b5a659631179c09776df4f65c2e15f9db01
SergSerg <sergio.ne@inbox.ru>
2020-05-20 11:20:37 +0600

    Создал стиль для кнопки Show Weather

---
+++ WeatherAppMaterial/app/src/main/java/com/example/weatherappmaterial/data/WeatherRequestList.java
@@ -0,0 +1,19 @@
+package com.example.weatherappmaterial.data;
+
+
+
+
+public class WeatherRequestList {
+
+    WeatherRequest [] list;
+
+    public WeatherRequest[] getList() {
+        return list;
+    }
+
+    public void setList(WeatherRequest[] list) {
+        this.list = list;
+    }
+
+
+}
