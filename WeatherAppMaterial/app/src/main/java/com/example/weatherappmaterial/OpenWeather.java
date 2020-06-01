22c03e8d484c8c2a10da747db9be9deceb3c36a8
SergSerg <sergio.ne@inbox.ru>
2020-06-01 15:42:59 +0600

    Создал интерфейс OpenWeather

---
+++ WeatherAppMaterial/app/src/main/java/com/example/weatherappmaterial/OpenWeather.java
@@ -0,0 +1,12 @@
+package com.example.weatherappmaterial;
+
+import com.example.weatherappmaterial.data.WeatherRequest;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface OpenWeather {
+      @GET("data/2.5/find")
+      Call<WeatherRequest> loadWeather(@Query("q") String cityName, @Query("units") String metric, @Query("appid") String keyApi);
+}
