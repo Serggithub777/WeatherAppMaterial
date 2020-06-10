c636b38102e4b245067a625de5500e784312d589
SergSerg <sergio.ne@inbox.ru>
2020-06-10 14:25:21 +0600

    Создал объект доступа к данным WeatherDao

---
+++ WeatherAppMaterial/app/src/main/java/com/example/weatherappmaterial/dao/WeatherDao.java
@@ -0,0 +1,29 @@
+package com.example.weatherappmaterial.dao;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.weatherappmaterial.model.WeatherHistory;
+
+import java.util.List;
+
+public interface WeatherDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertWeatherHistory(WeatherHistory weatherHistory);
+
+    @Update
+    void updateWeatherHistory(WeatherHistory weatherHistory);
+
+    @Delete
+    void deleteWeatherHistory(WeatherHistory weatherHistory);
+
+    @Query("select * from weatherhistory")
+    List<WeatherHistory> getAllWeatherHistory();
+
+    @Query("select count() from weatherhistory")
+    long getCountWeatherHistory();
+
+}
