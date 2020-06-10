24ee9215f2e05e9c15c29576bb12da3ddfe9951f
SergSerg <sergio.ne@inbox.ru>
2020-06-10 23:10:02 +0600

    Добавил метод initHistoryRecyclerView класса HistoryFragment

--- WeatherAppMaterial/app/src/main/java/com/example/weatherappmaterial/HistoryFragment.java
+++ WeatherAppMaterial/app/src/main/java/com/example/weatherappmaterial/HistoryFragment.java
@@ -1,5 +1,6 @@
 package com.example.weatherappmaterial;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +9,38 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.weatherappmaterial.dao.WeatherDao;
 
 public class HistoryFragment extends Fragment {
+    private HistoryRecyclerAdapter adapter;
+    private WeatherSource weatherSource;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initHistoryRecyclerView(view);
+    }
+
+    private void initHistoryRecyclerView(View view) {
+        RecyclerView recyclerView = view.findViewById(R.id.historyRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        WeatherDao weatherDao = App.getInstance().getWeatherDao();
+        weatherSource = new WeatherSource(weatherDao);
+
+        adapter = new HistoryRecyclerAdapter(this, weatherSource);
+        recyclerView.setAdapter(adapter);
+
     }
 }
