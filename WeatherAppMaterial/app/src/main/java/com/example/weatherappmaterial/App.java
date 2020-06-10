866b135b58120be9b1bca6e515f86b0d46d07e26
SergSerg <sergio.ne@inbox.ru>
2020-06-10 14:40:17 +0600

    Создал класс App c инициализацией синглтона БД

---
+++ WeatherAppMaterial/app/src/main/java/com/example/weatherappmaterial/App.java
@@ -0,0 +1,31 @@
+package com.example.weatherappmaterial;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.example.weatherappmaterial.dao.WeatherDao;
+
+public class App extends Application {
+    private static App instance;
+    private WeatherAppDatabase wad;
+
+    public static App getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        wad = Room.databaseBuilder(
+                getApplicationContext(),
+                WeatherAppDatabase.class,
+                "weather_app_database")
+                .build();
+    }
+
+    public WeatherDao getWeatherDao() {
+        return  wad.getWeatherDao();
+    }
+}
